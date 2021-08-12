89c137c7982df936b89f8f4193ade14cb4023114
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2021-08-12 15:58:08 +0300

    if-else-ternary yapisi

---
+++ if-else-if/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın");
+            else if (time <= 18)
+                Console.WriteLine("İyi günler");
+            else
+                Console.WriteLine("İyi geceler");
+
+            //ternary if
+            // ?'den sonraki kisim if
+            // :'den sonraki kisim else
+            string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+
+            //2 ternary if yani if-else if-else yapisi
+            sonuc = time >= 6 && time <= 11 ? "Günaydın"
+            : time <= 18 ? "İyi günler" : "İyi geceler";
+
+            bool condition = true;
+            if (condition)
+            {
+                Console.WriteLine("Değişken: true");
+            }
+            else
+            {
+                Console.WriteLine("Değişken: false");
+            }
+        }
+    }
+}
