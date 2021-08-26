f3643eb9e162d5ff4c8a9abc3b5430e2059be923
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2021-08-26 16:15:05 +0300

    add project files

---
+++ SOLID/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface ILogger
+    {
+        bool Log(string value);
+    }
+
+    class XmlLog : ILogger
+    {
+        public bool Log(string value)
+        {
+            //
+            //
+            return true;
+        }
+    }
+    class Logger
+    {
+        ILogger _logger;
+
+        public Logger(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Log(string value)
+        {
+            _logger.Log(value);
+        }
+    }
+    
+}
